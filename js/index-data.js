7247bd6bbb4e06a002c3b0578359c554aabea39c
xjxhub <32325028+xjxhub@users.noreply.github.com>
2017-10-17 05:18:46 -0500

    10.17
    1.下拉菜单的箭头，字体，向下移动，cursor；
    2.统一nav；
    3.PPT窗口大小以及全屏设置；
    4.banner左侧字体，背景加宽；
    5.菜单及排行榜实现hover；
    6.hiddenbox中内容；
    7.删除课程详情一行，；
    8.点击图片或点击title跳转至不同（带树形结构或否）的课程详情页面；
    9.课程详情页面的添加；

---
+++ js/index-data.js
@@ -0,0 +1,141 @@
+var indexdata={
+    "id":["dropdownMenu1","dropdownMenu2","dropdownMenu3","dropdownMenu4","dropdownMenu5"],
+    "navs":[
+        {
+            "first":"教学中心",
+            "firsthref":"all树形二级.html",
+            "second":["汽车发动机","汽车底盘","汽车电气","汽车变速器","新能源汽车"],
+            "third":[
+               {"a0": ["机械系统","电控系统"]},
+               {"a1": ["传动","行驶","制动","转向"]},
+               {"a2": ["电源","启动","照明","辅助","仪表","空调"]},
+               {"a3": ["自动变速器","手动变速器","手自一体变速器","CVT变速器","DSG双离合变速器","E-CVT变速器"]},
+               {"a4": ["纯电动汽车","混合动力汽车"]}
+                    ]
+        },
+        {
+            "first":"中心教学",
+            "second":["发动机","底盘","电气","变速器","源汽车"],
+            "third":[
+               {"a0": ["机统","电统"]},
+               {"a1": ["传动","行驶","制动","转向"]},
+               {"a2": ["电源","启动","照明","辅助","仪表","空调"]},
+               {"a3": ["自动器","手动器","手自一体器","CVT器","DSG双离速器","E-CVT变速器"]},
+               {"a4": ["纯电动汽车","混合动力汽车"]}
+                    ]
+        },
+        {
+            "first":"教心学中",
+            "second":["汽车","汽盘","汽气","汽速器","新能车"],
+            "third":[
+               {"a0": ["机械系统","电控系统"]},
+               {"a1": ["传动","行驶","制动","转向"]},
+               {"a2": ["电源","启动","照明","辅助","仪表","空调"]},
+               {"a3": ["自动变速器","手动变速器","手自一体变速器","CVT变速器","DSG双离合变速器","E-CVT变速器"]},
+               {"a4": ["纯电动汽车","混合动力汽车"]}
+            ]
+        },
+        {
+            "first":"教学心中",
+            "second":["汽车发动机","汽车底盘","汽车电气","汽车变速器","新能源汽车"],
+            "third":[
+               {"a0": ["机械系统","电控系统"]},
+               {"a1": ["传动","行驶","制动","转向"]},
+               {"a2": ["电源","启动","照明","辅助","仪表","空调"]},
+               {"a3": ["自动变速器","手动变速器","手自一体变速器","CVT变速器","DSG双离合变速器","E-CVT变速器"]},
+               {"a4": ["纯电动汽车","混合动力汽车"]}
+                    ]
+        },
+        {
+            "first":"教中心学",
+            "second":["汽车发动机","汽车底盘","汽车电气","汽车变速器","新能源汽车"],
+            "third":[
+               {"a0": ["机械系统","电控系统"]},
+               {"a1": ["传动","行驶","制动","转向"]},
+               {"a2": ["电源","启动","照明","辅助","仪表","空调"]},
+               {"a3": ["自动变速器","手动变速器","手自一体变速器","CVT变速器","DSG双离合变速器","E-CVT变速器"]},
+               {"a4": ["纯电动汽车","混合动力汽车"]}
+                    ]
+        }
+        ]
+};
+
+
+
+
+
+
+var videoData=[
+    {
+        "num": "intro1",
+        "numi":"intro11",
+        "images":"imgs/低压蓄电池警示灯亮的检修da.jpg",
+        "title":"低压蓄电池警示灯亮的检修",
+        "href":"video-html/video-前氧传感器故障检测.html",
+        "titlehref":"不带树形的二级.html",
+        "introduce":"简介1",
+        "ppt":"ppt0"
+    },
+    {
+        "num": "intro2",
+        "numi":"intro12",
+        "images":"imgs/前氧传感器故障检修da.jpg",
+        "title":"前氧传感器故障检修",
+        "href":"video-html/video.html",
+        "titlehref":"不带树形的二级.html",
+        "introduce":"简介2",
+        "ppt":"ppt1"
+    },
+    {
+        "num": "intro3",
+        "numi":"intro13",
+        "images":"imgs/更换汽车多媒体系统da.jpg",
+        "title":"更换汽车多媒体系统",
+        "href":"video-html/video.html",
+        "titlehref":"不带树形的二级.html",
+        "introduce":"简介3",
+        "ppt":"ppt2"
+    },
+    {
+        "num": "intro4",
+        "numi":"intro14",
+        "images":"imgs/混合动力发动机故障灯常亮的检修da.jpg",
+        "title":"混合动力发动机故障灯常亮的检修",
+        "href":"video-html/video.html",
+        "titlehref":"不带树形的二级.html",
+        "introduce":"简介4",
+        "ppt":"ppt3"
+    },
+    {
+        "num": "intro5",
+        "numi":"intro15",
+        "images":"imgs/混合动力汽车安全指导手册da.jpg",
+        "title":"混合动力汽车安全指导手册",
+        "href":"video-html/video.html",
+        "titlehref":"不带树形的二级.html",
+        "introduce":"简介5",
+        "ppt":"ppt4"
+    },
+    {
+        "num": "intro6",
+        "numi":"intro16",
+        "images":"imgs/更换雨刮总成da.jpg",
+        "title":"更换雨刮总成",
+        "href":"video-html/video.html",
+        "titlehref":"不带树形的二级.html",
+        "introduce":"简介6",
+        "ppt":"ppt5"
+    },
+    {
+        "num": "intro7",
+        "numi":"intro17",
+        "images":"imgs/前氧传感器故障检修da.jpg",
+        "title":"前氧传感器故障检修",
+        "href":"video-html/video.html",
+        "titlehref":"不带树形的二级.html",
+        "introduce":"简介7",
+        "ppt":"ppt6"
+    }
+];
+
+
